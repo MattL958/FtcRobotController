3b5d8333e58da0b3b63f5c7b8437c66ae36cb6c7
MattL958 <matthew.ryan.liong@gmail.com>
2026-01-16 17:32:03 -0500

    i hate pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpRed.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
@@ -10,30 +11,38 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
 
+@Config
 @TeleOp
 public class TeleOpRed extends OpMode {
     MecanumDrive drive = new MecanumDrive(); //call class
     double forward, strafe, rotate;
     private DcMotor intake;
+    private DcMotor shooting;
     private IMU imu;
     private Limelight3A limelight;
     private CRServo turretServo;
 
     double error;
+    double last_error = 0.0;
+    double derivative, integral;
 
     double turretPower;
-    double kp,kd,ki;
-
-    TelemetryPacket packet = new TelemetryPacket();
+    public static double kp,kd,ki; //public static shows up in dashboard config
+    private ElapsedTime deltaTime = new ElapsedTime();
+    double[] errorArr = new double[10];
+    double sum;
 
     FtcDashboard dashboard = FtcDashboard.getInstance();
+
 
 
     @Override
@@ -44,15 +53,20 @@ public class TeleOpRed extends OpMode {
 
         //init drive/imu
         drive.init(hardwareMap);
-        imu = hardwareMap.get(IMU.class, "imu");
 
 
         //init motors
-        intake = hardwareMap.get(DcMotorEx.class,"intake");
+        intake = hardwareMap.get(DcMotor.class,"intake");
 
         intake.setDirection(DcMotor.Direction.FORWARD);
 
         intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        shooting = hardwareMap.get(DcMotorEx.class, "shooting");
+
+        shooting.setDirection((DcMotorSimple.Direction.FORWARD));
+
+        shooting.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         //init limelight
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
@@ -103,32 +117,101 @@ public class TeleOpRed extends OpMode {
         telemetry.addData("Tag Count", llResult.getFiducialResults().size());
 
 
+
+
         if (llResult != null && llResult.isValid()){
             Pose3D botPose = llResult.getBotpose_MT2();
             telemetry.addData("tx", llResult.getTx());
             telemetry.addData("ty",llResult.getTy());
             telemetry.addData("ta", llResult.getTa());
             telemetry.addData("BotPose", botPose.toString());
+
+            error = -llResult.getTx();
+
+            derivative = (error-last_error)/deltaTime.seconds();
+            if (Math.abs(error)<0.5){
+                derivative=0.0;
+            }
+
+            for(int i = 0; i<errorArr.length;i++){
+                if (errorArr[i] == 0){
+                    errorArr[i] = error;
+                    break;
+                }
+
+                if(i==errorArr.length-1){
+                    for(int j = 0; j<errorArr.length-1;j++){
+                        errorArr[j] = errorArr[j+1];
+                    }
+                    errorArr[errorArr.length-1] = error;
+                }
+            }
+
+
+
+            sum = 0;
+            for(int i = 0; i<errorArr.length;i++){
+                sum += errorArr[i];
+            }
+
+            error = sum/errorArr.length;
         } else {
             telemetry.addData("No Tag Found","");
+            error = 0.0;
         }
 
 
 
         //turret PID
-        kp=0.1;
 
-        error = llResult.getTx();
-        turretPower = kp*error;
-        //remap
-        turretPower = turretPower/(25*kp); //max error should be 25 degrees, so this should normalize. results in a value between -1 and 1
 
-        if(Math.abs(turretPower)>1){
-            turretPower = Math.abs(turretPower)/turretPower;
+        kp=0.03;
+        kd=0.003;
+        ki=0;
+
+        if(last_error==0.0){
+            last_error=error;
         }
 
+        telemetry.addData("error",error);
+
+
+        integral += error * deltaTime.seconds();
+
+        turretPower = kp*error + kd*derivative + ki*integral;
+        telemetry.addData("dt",deltaTime.seconds());
+        telemetry.addData("error-last_error = ",error-last_error);
+        telemetry.addData("p",kp*error);
+        telemetry.addData("d",kd*derivative);
+        telemetry.addData("i",ki*integral);
+
+        deltaTime.reset();
+        last_error=error;
+
+        //clamp
+        if(turretPower>1.0){
+            turretPower = 1.0;
+        } else if (turretPower<-1.0) {
+            turretPower = -1.0;
+        } else if (Math.abs(turretPower)<0.2) {
+            turretPower = 0;
+        }
+
+        if (!(llResult != null && llResult.isValid())){
+            turretPower=-0.02;
+        }
+
+        //send power
+        telemetry.addData("turretPower",turretPower);
         turretServo.setPower(turretPower);
 
+        shooting.setPower(gamepad1.left_trigger);
+
+
+        TelemetryPacket packet = new TelemetryPacket(); //create a new packet each loop
         packet.put("error",error);
+
+
+        dashboard.sendTelemetryPacket(packet);
     }
 }
